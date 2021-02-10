607c6f8af27d923dc96f1b4f793c9879763bbb54
mzaink <sidsmith9@gmail.com>
2021-02-10 11:53:10 +0530

    Set up project | FC

---
+++ android/app/src/main/kotlin/com/mzaink/grpc_playground/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mzaink.grpc_playground
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class GrpcPlaygroundApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp();
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('GRPC Playground'),
+        ),
+      );
+}
